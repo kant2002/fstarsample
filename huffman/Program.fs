2e1a9976c7a8592ec8791c8503962b87027ec9d0
Andrii Kurdiumov <kant2002@gmail.com>
2022-08-07 14:16:37 +0600

    Make sure that message works properly Huffman sample on Windows should use "\r\n", so I use .NET idiom

--- huffman/Program.fs
+++ huffman/Program.fs
@@ -1,6 +1,7 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 open Huffman
 open FStar_IO
+open System
 
 let encoding = [('A', 2); ('E',5); ('F',1); ('H',1); ('I',1); ('L', 1);
    ('N',1); ('R',1); ('S',2); ('T',3); ('U',1); (' ',2)];
@@ -13,8 +14,8 @@ let decodedData = match encodedData with
     | FStar_Pervasives_Native.Some(e) -> decode t e
     | _ -> failwith "Failed to encode data"
 let message = match decodedData with
-    | FStar_Pervasives_Native.Some(d) -> (System.String.Concat(Array.ofList(d)) + "\n")
+    | FStar_Pervasives_Native.Some(d) -> (System.String.Concat(Array.ofList(d)) + Environment.NewLine)
     | _ -> failwith "Failed to decode data"
-print_string "After roundtip, value of 'THE ESSENTIAL FEATURE' is:\n";;
+print_string ("After roundtip, value of 'THE ESSENTIAL FEATURE' is:" + Environment.NewLine);;
 print_string message;;
 print_string "You can remove any letter from 'encoding' variable in the sample, to make it fails."
